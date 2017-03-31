4529fea832ae957505ad18825d64fa808d861ebe
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-03-31 12:01:30 +0100

    can now add connections based on what will be serial data from arduino

--- app/Socket.js
+++ app/Socket.js
@@ -3,6 +3,7 @@ class Socket {
     this.type = type;
     this.node = node;
     this.pin = pin;
+    this.connectedTo = [];
     Socket.registerSocket(this);
   }
 
@@ -18,14 +19,59 @@ class Socket {
     return foundSocket;
   }
 
+  static testConnection(pin1, pin2) {
+    var socket1 = Socket.getSocketFromPin(pin1);
+    var socket2 = Socket.getSocketFromPin(pin2);
+    var outSocket, inSocket;
+    var valid = true;
+    if(socket1.type == Socket.OUT && socket2.type == Socket.IN) {
+      outSocket = socket1;
+      inSocket = socket2;
+    } else if(socket1.type == Socket.IN && socket2.type == Socket.OUT) {
+      outSocket = socket2;
+      inSocket = socket1;
+    } else {
+      valid = false;
+    }
+    var returnObject = {
+      valid: valid
+    }
+    if(valid) {
+      returnObject.out = outSocket.pin;
+      returnObject.in = inSocket.pin;
+      returnObject.exists = (outSocket.connectedTo.indexOf(inSocket.pin)>=0);
+    }
+    return returnObject;
+  }
+
   static makeConnection(pin1, pin2) {
     var socket1 = Socket.getSocketFromPin(pin1);
     var socket2 = Socket.getSocketFromPin(pin2);
+    var outSocket, inSocket;
+    var valid = true;
     if(socket1.type == Socket.OUT && socket2.type == Socket.IN) {
-      socket1.node.connect(socket2.node);
+      outSocket = socket1;
+      inSocket = socket2;
     } else if(socket1.type == Socket.IN && socket2.type == Socket.OUT) {
-      socket2.node.connect(socket1.node);
+      outSocket = socket2;
+      inSocket = socket1;
+    } else {
+      valid = false;
     }
+    if(valid && outSocket.connectedTo.indexOf(inSocket.pin)==-1) {
+      outSocket.node.connect(inSocket.node);
+      outSocket.connectedTo.push(inSocket.pin);
+    }
+  }
+
+  static getConnections() {
+    var connections = [];
+    var s;
+    for(var i = 0; i < Socket._sockets.length; i ++) {
+      s = Socket._sockets[i];
+      connections[s.pin] = s.connectedTo;
+    }
+    return connections;
   }
 }
 
--- app/main.js
+++ app/main.js
@@ -3,4 +3,26 @@ var actx = new AudioContext();
 var vco1 = new VCO(0,1,2,3);
 var out = new Output(4);
 
-Socket.makeConnection(0,4);
+function updateConnections(data) {
+  var i,j;
+  var previousConnections = Socket.getConnections();
+  var connectionsToAdd = [];
+
+  var splitData = data.split(",");
+  var allValid = true;
+  var c, res;
+  for(i = 0; i < splitData.length; i ++) {
+    c = splitData[i].split("-");
+    if(c.length == 2) {
+      c[0] = parseInt(c[0]);
+      c[1] = parseInt(c[1]);
+      console.log(c);
+      res = Socket.testConnection(c[0],c[1]);
+      if(!res.valid) allValid = false;
+      else if(!res.exists) Socket.makeConnection(c[0], c[1]);
+    }
+  }
+  console.log(allValid?"all connections ok":"check connections");
+}
+
+updateConnections("0-4");
