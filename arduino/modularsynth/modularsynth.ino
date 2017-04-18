9f14077252a8b8c4a27b271231e77ff635fa2ab7
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-04-18 18:33:32 +0100

    incorporated midi input into main arduino sketch but needs tidying up

--- arduino/modularsynth/modularsynth.ino
+++ arduino/modularsynth/modularsynth.ino
@@ -1,3 +1,8 @@
+byte incomingByte;
+byte note;
+byte velocity;
+int action=2; //0 =note off ; 1=note on ; 2= nada
+
 // pins for selecting mux channel for reading
 const int READ_SELECT_1 = 2;
 const int READ_SELECT_2 = 3;
@@ -29,6 +34,7 @@ void setup() {
   pinMode(WRITE_SELECT_2,OUTPUT);
   pinMode(WRITE_SELECT_3,OUTPUT);
   Serial.begin(9600);
+  Serial1.begin(31250);
 }
 
 void loop() {
@@ -52,6 +58,42 @@ void loop() {
       }
     }
   }
-  Serial.println("DONE");
-  delay(100);
+  //Serial.println("DONE");
+  if (Serial1.available() > 0) {
+   // read the incoming byte:
+   incomingByte = Serial1.read();
+
+   // wait for as status-byte, channel 1, note on or off
+   if (incomingByte== 144){ // note on message starting starting
+     action=1;
+   }else if (incomingByte== 128){ // note off message starting
+     action=0;
+   }else if (incomingByte== 208){ // aftertouch message starting
+      //not implemented yet
+   }else if (incomingByte== 160){ // polypressure message starting
+      //not implemented yet
+   }else if ( (action==0)&&(note==0) ){ // if we received a "note off", we wait for which note (databyte)
+     note=incomingByte;
+     playNote(note, 0);
+     note=0;
+     velocity=0;
+     action=2;
+   }else if ( (action==1)&&(note==0) ){ // if we received a "note on", we wait for the note (databyte)
+     note=incomingByte;
+   }else if ( (action==1)&&(note!=0) ){ // ...and then the velocity
+     velocity=incomingByte;
+     playNote(note, velocity);
+     note=0;
+     velocity=0;
+     action=0;
+   }else{
+     //nada
+   }
+  }
+  //delay(100);
+}
+
+void playNote(byte note, byte velocity){
+  Serial.println("PLAYED NOTE");
+  Serial.println(note);
 }
