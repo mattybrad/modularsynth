09d45a7a05b0ab17815ec1cb3695607b15400006
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-05-07 12:42:54 +0100

    phaser

--- app/GUI/GUI.js
+++ app/GUI/GUI.js
@@ -26,4 +26,15 @@ class GUI {
   }
 }
 
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gui', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
+
 GUI.gui = new GUI();
