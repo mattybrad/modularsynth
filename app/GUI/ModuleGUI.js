7e60baeaa93a35b1f3040722993b1df8cdb99ccd
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-05-16 21:28:34 +0100

    some gui stuff

--- app/GUI/ModuleGUI.js
+++ app/GUI/ModuleGUI.js
@@ -1,6 +1,7 @@
 class ModuleGUI extends Phaser.Group {
   constructor(phaser, module, x, y, width) {
     super(phaser);
+    //phaser.add.existing(this);
     this.phaser = phaser;
     this.x = x;
     this.y = y;
